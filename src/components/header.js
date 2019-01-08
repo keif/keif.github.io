d2bdc91c5cd331418b18e29f2ee884dd96b475d8
Keith T. Baker <hello@keif.name>
2019-01-08 00:50:22 -0500

    remove console.log

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import styles from './header.module.scss'
-console.log(styles)
+
 const Header = ({ siteTitle }) => (
   <header className={styles.header}>
     <h1>
