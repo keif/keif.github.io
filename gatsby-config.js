1107e6a22c9ae2dd870a2ff32ae161391a27b119
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-01-15 02:34:04 -0500

    gatsby-blog: adding new packages for gatbsy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Personal blog"
+    title: 'Personal blog',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -8,35 +8,49 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/posts`,
-        name: `posts`
-      }
+        name: `posts`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
-        name: `about`
-      }
+        name: `about`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
+            resolve: 'gatsby-remark-embed-gist',
+            options: {
+              username: 'keif',
+              includeDefaultCss: true,
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
-            classPrefix: "language-",
+            classPrefix: 'language-',
             inlineCodeMarker: null,
-            aliases: {}
-          }
-        ]
-      }
+            aliases: {},
+          },
+        ],
+      },
     },
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
-    }
-  ]
-};
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+      },
+    },
+  ],
+}
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,52 +1,52 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import { graphql } from "gatsby";
-import BlogPage from "../blocks/Post";
-import Layout from "../components/layout";
+import Layout from '../components/layout';
+import BlogPage from '../blocks/Post';
+
+import { graphql } from 'gatsby';
 
 export default function Template({ data }) {
-  const { markdownRemark: post } = data;
+    const { markdownRemark: post } = data;
 
-  return (
-    <Layout>
-      <BlogPage>
-        <BlogPage.Info>
-          <BlogPage.Date>{post.frontmatter.date}</BlogPage.Date>
-          <BlogPage.Data>
-            {post.timeToRead} {post.timeToRead > 1 ? "minutes" : "minute"} read
-            time
-            <span>-</span>
-            {post.frontmatter.category}
-          </BlogPage.Data>
-        </BlogPage.Info>
-        <BlogPage.Text>
-          <h1>{post.frontmatter.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </BlogPage.Text>
-      </BlogPage>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <BlogPage>
+                <BlogPage.Info>
+                    <BlogPage.Date>{post.frontmatter.date}</BlogPage.Date>
+                    <BlogPage.Data>
+                        {post.timeToRead} {post.timeToRead > 1 ? 'minutes' : 'minute'} read time
+                        <span>-</span>
+                        {post.frontmatter.category}
+                    </BlogPage.Data>
+                </BlogPage.Info>
+                <BlogPage.Text>
+                    <h1>{post.frontmatter.title}</h1>
+                    <div dangerouslySetInnerHTML={{ __html: post.html }} />
+                </BlogPage.Text>
+            </BlogPage>
+        </Layout>
+    );
 }
 
 Template.propTypes = {
-  data: PropTypes.object
+    data: PropTypes.object,
 };
 Template.defaultProps = {
-  data: null
+    data: null,
 };
 
 export const postQuery = graphql`
-  query BlogPostByPath($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      timeToRead
-      frontmatter {
-        path
-        title
-        date
-        category
-      }
+    query BlogPostByPath($path: String!) {
+        markdownRemark(frontmatter: { path: { eq: $path } }) {
+            html
+            timeToRead
+            frontmatter {
+                path
+                title
+                date
+                category
+            }
+        }
     }
-  }
 `;
