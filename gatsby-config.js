6c6e448bf120e6bfe8e70d6ccf02d69686125c9d
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-02-07 22:56:23 -0500

    gatsby-blog: formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,56 +1,56 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Personal blog',
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/posts`,
-        name: `posts`,
-      },
+    siteMetadata: {
+        title: "Personal blog",
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: `about`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-embed-gist',
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              username: 'keif',
-              includeDefaultCss: true,
+                path: `${__dirname}/src/posts`,
+                name: `posts`,
             },
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            classPrefix: 'language-',
-            inlineCodeMarker: null,
-            aliases: {},
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        useMozJpeg: false,
-        stripMetadata: true,
-      },
-    },
-  ],
-}
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: `about`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: "gatsby-remark-embed-gist",
+                        options: {
+                            username: "keif",
+                            includeDefaultCss: true,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        classPrefix: "language-",
+                        inlineCodeMarker: null,
+                        aliases: {},
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography.js`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                useMozJpeg: false,
+                stripMetadata: true,
+            },
+        },
+    ],
+};
