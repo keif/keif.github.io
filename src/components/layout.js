dd1978c88cf9aa55b04ee858f7978b4ddb45cfde
Rev. Keith T. Baker <keith_baker@abercrombie.com>
2019-02-22 09:34:35 -0500

    gatsby-blog: add google header meta tag

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,30 +10,33 @@ require("typeface-raleway");
 require("typeface-roboto-slab");
 
 const TemplateWrapper = ({ children }) => (
-  <div
-    style={{
-      display: "grid",
-      gridTemplateColumns: "100%",
-      gridTemplateRows: "60px 1fr auto"
-    }}
-  >
-    <Helmet
-      title="Keith Baker - Developer and Digital Engineer"
-      meta={[
-        { name: "description", content: "Keith Baker - Developer and Digital Engineer" },
-        {
-          name: "keywords",
-          content:
-            "front-end, design, developer, minimal, gatsby, keith, baker, ecommerce, web development"
-        }
-      ]}
-      link={[{ rel: "shortcut icon", type: "image/png", href: `${favIcon}` }]}
+    <div
+        style={{
+            display: "grid",
+            gridTemplateColumns: "100%",
+            gridTemplateRows: "60px 1fr auto",
+        }}
     >
-      <html lang="en" />
-    </Helmet>
-    <Navigation />
-    {children}
-  </div>
+        <Helmet
+            title="Keith Baker - Developer and Digital Engineer"
+            meta={[
+                { name: "description", content: "Keith Baker - Developer and Digital Engineer" },
+                {
+                    name: "keywords",
+                    content: "front-end, design, developer, minimal, gatsby, keith, baker, ecommerce, web development",
+                },
+                {
+                    name: "google-site-verification",
+                    content: "ojxzHz13Q3TNqNO0VsnyxLDpDb1NL1FIMMQG0GwIIqc",
+                },
+            ]}
+        link={[{ rel: "shortcut icon", type: "image/png", href: `${favIcon}` }]}
+        >
+            <html lang="en" />
+        </Helmet>
+        <Navigation />
+        {children}
+    </div>
 );
 
 export default TemplateWrapper;
