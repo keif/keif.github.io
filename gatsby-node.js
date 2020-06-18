9af6eb3c2e63c4fbf85cc683db4e44f62b1e256f
Keith T. Baker <hello@keif.name>
2020-06-18 05:54:05 -0400

    update packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,44 +1,44 @@
 const path = require("path");
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+    const { createPage } = actions;
 
-  return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-            id
-            frontmatter {
-              page
-              path
-              title
-              date
+    return graphql(`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        html
+                        id
+                        frontmatter {
+                            page
+                            path
+                            title
+                            date
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-  `).then(res => {
-    if (res.errors) {
-      return Promise.reject(res.errors);
-    }
+    `).then(res => {
+        if (res.errors) {
+            return Promise.reject(res.errors);
+        }
 
-    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      if (node.frontmatter.page) {
-        createPage({
-          path: node.frontmatter.path,
-          component: path.resolve("src/templates/about.js"),
-          layout: "about"
+        res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            if (node.frontmatter.page) {
+                createPage({
+                    path: node.frontmatter.path,
+                    component: path.resolve("src/templates/about.js"),
+                    layout: "about",
+                });
+            } else {
+                createPage({
+                    path: node.frontmatter.path,
+                    component: path.resolve("src/templates/post.js"),
+                    layout: "post",
+                });
+            }
         });
-      } else {
-        createPage({
-          path: node.frontmatter.path,
-          component: path.resolve("src/templates/post.js"),
-          layout: "post"
-        });
-      }
     });
-  });
 };
