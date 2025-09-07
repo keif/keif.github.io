7f3e040f9dea66f9a176e925b78a5359db4726b4
Keith Baker <github@baker.is>
2025-09-07 03:58:56 -0400

    feat(blog): moving to collections

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.coerce.date(),
+    description: z.string(),
+    author: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
