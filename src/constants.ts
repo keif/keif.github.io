2191e1f7b5f85edd799bccf70ac860b615a4ad05
Keith Baker <github@baker.is>
2025-09-09 22:16:53 -0400

    fix(socials): update social media

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,14 @@
-import type { Props } from "astro";
-import IconMail from "@/assets/icons/IconMail.svg";
-import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconBluesky from "@/assets/icons/IconBluesky.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
-import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import IconMail from "@/assets/icons/IconMail.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import { SITE } from "@/config";
+import type { Props } from "astro";
 
 interface Social {
   name: string;
@@ -19,25 +20,31 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/keif",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
+    name: "Bluesky",
+    href: "https://bsky.app/profile/keifers.bsky.social",
+    linkTitle: `${SITE.title} on Bluesky`,
+    icon: IconBluesky,
+  },
+  {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/ikeif",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/keithbaker/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:blog@baker.is",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
